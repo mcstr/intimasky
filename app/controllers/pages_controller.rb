f4f68a33686c212724b786a6e9dfbd4271715cf5
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-27 16:48:22 +0100

    adding dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
+  def dashboard
+    @user = current_user
+    @bookings = @user.bookings
+    @mask = Mask.find(@bookings[0][:mask_id])
+
+  end
   def home
   end
   def search
