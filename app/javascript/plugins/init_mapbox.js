71745b8fe79bb51cac2b2152fd9d8b22e8a52ab5
mcstr <quecontentoestoy@gmail.com>
2021-02-10 16:50:08 +0100

    change mapbox style

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,7 +6,7 @@ const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
+    style: 'mapbox://styles/bmchavez1991/ck759mmsp0ath1imoksa292rw',
   });
 };
 
