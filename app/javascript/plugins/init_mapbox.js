1aa5d0cdea4164ee8315942aa94c157033499593
mcstr <quecontentoestoy@gmail.com>
2021-02-10 16:41:50 +0100

    redo responsiveness

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,22 +6,19 @@ const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    // style: 'mapbox://styles/mapbox/streets-v10'
-    style: 'mapbox://styles/bmchavez1991/ck759mmsp0ath1imoksa292rw'
+    style: 'mapbox://styles/mapbox/streets-v10',
   });
 };
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(map);
+    new mapboxgl.Marker().setLngLat([marker.lng, marker.lat]).addTo(map);
   });
 };
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
