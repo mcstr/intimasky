dc6219e567f0e83aedaf7f617c3df7ba68f4636e
mcstr <quecontentoestoy@gmail.com>
2021-02-10 21:53:35 +0100

    update mapbox styles

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,7 +6,7 @@ const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/bmchavez1991/ck759mmsp0ath1imoksa292rw',
+    style: 'mapbox://styles/mapbox/dark-v10',
   });
 };
 
